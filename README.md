35269db83a020836955eaf65b17516810c601198
Winga <laba_w@etna-alternance.net>
2023-04-25 21:49:19 +0200

    todo list fini

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # TODO-list-
+
+Etape 1 
+
+Lancer le projet 
+
+cd TODO-list-
+
+npm install
+
+npm run dev
+
+
+Etape 2 
+
+Lancer le serveur json 
+
+cd backend 
+
+json-server --watch db.json --port 3000
+
+
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const journals = await fetch('http://localhost:8081/journal').then(journal => journal.json())
-let n = parseInt(localStorage.getItem("idTitre"));
-let nb = journals[n].info.length;
+const journals = await fetch('http://localhost:3000/journal').then(journal => journal.json())
+const last_page = journals.length;
+if (journals.length == 0) {
+    localStorage.setItem("idTitre", 1)
+}
+else if(localStorage.getItem('idTitre') == null){
+    localStorage.setItem("idTitre", last_page)
+}
+     
+let page = parseInt(localStorage.getItem('idTitre'))-1;
 
-function GenerateTodoList() {
+GET_Todolist(page)
+
+function GenerateTodoList(nb) {
   if(nb < 13) {
   const divCreateTodoElement = document.querySelector(".todolistGenerate"); 
   const divCheckbox = document.createElement("div");
@@ -10,6 +19,7 @@ function GenerateTodoList() {
   inputText.type = "text";
   inputText.id = nb;
   inputText.name = "todo"; 
+  inputText.placeholder = "ajouter une tâche"
   const inputCheckbox = document.createElement("input");
   inputCheckbox.type = "checkbox";
   inputCheckbox.id = nb; 
@@ -22,10 +32,17 @@ function GenerateTodoList() {
 
 function EventaddTodoList() {
     const btnAddTodoList = document.querySelector(".btn-add-todolist");
+    let nb = 0;
+    if(page >= 0 && page < journals.length)
+       nb = journals[page].info.length
+    
     btnAddTodoList.addEventListener("click", function(event){
        event.preventDefault();
-       GenerateTodoList();
+       nb+=1
+       GenerateTodoList(nb)
     })
+    
+    
 }
 
 function POST_Todolist(){
@@ -33,19 +50,20 @@ function POST_Todolist(){
     const titreElement = document.querySelector('#Title-of-Todo-list');
     todolistElement.addEventListener('submit', function(event){
         event.preventDefault();
-        localStorage.setItem('idTitre', journals.length+1);
+
         const arrayTarget = event.target.querySelectorAll("[name=todo]");
         let arrayTodoList= []
         for(let i =0 ; i < arrayTarget.length; i+=1){
             arrayTodoList.push({id_todo : i , tâches :  arrayTarget[i].value, status_termine : false});
         }
        const todolist = {
-            id : journals.length+1,
+            id : last_page+1,
             titre : titreElement.value,
             info : arrayTodoList
         }
         const chargeUtile = JSON.stringify(todolist);
-        fetch('http://localhost:8081/journal', {
+        console.log(chargeUtile)
+        fetch('http://localhost:3000/journal', {
             method : "POST",
             headers : {"Content-Type": "application/json"},
             body : chargeUtile 
@@ -54,21 +72,26 @@ function POST_Todolist(){
 }
 
 function GET_Todolist(n_page){
+    
     const btnModifier = document.getElementById("modifier");
     const titleElement = document.querySelector('#Title-of-Todo-list');
-    const divTodoOne = document.querySelector(".entête");
-    if(localStorage.getItem('idTitre') != ""){
-        divTodoOne.innerHTML = "";
+    const btnRegister = document.querySelector('#register');
+    
+    const numero =  document.querySelector("#num");
+    numero.innerText = n_page+1;
+
+    if(n_page >= 0 && n_page < journals.length){
+        btnRegister.disabled = true;
+        btnRegister.style.cursor = "no-drop"
+        btnModifier.disabled = false;
+        btnModifier.style.cursor = "pointer";
         for(let u = 0 ; u <journals[n_page].info.length; u+=1){
             titleElement.value = journals[n_page].titre;
             const divCreateTodoElement = document.querySelector(".todolistGenerate"); 
-            const btnRegister = document.querySelector('#register');
-            btnRegister.disabled = true;
-            btnRegister.style.cursor = "no-drop"
             const deletebtn = document.createElement("button");
             deletebtn.className = "supprimer";
             const imgElement = document.createElement("img");
-            imgElement.src = "images/moins.png"
+            imgElement.src = "images/corbeille.png"
             const divCheckbox = document.createElement("div");
             const inputText = document.createElement("input");
             inputText.type = "text";
@@ -91,9 +114,11 @@ function GET_Todolist(n_page){
     }
     else{
         titleElement.value = "";
-        divTodoOne.lastElementChild.value = ""
         btnModifier.disabled = true;
         btnModifier.style.cursor = "no-drop";
+        btnRegister.disabled = false;
+        btnRegister.style.cursor = "pointer";
+        
     }
 }
 
@@ -103,15 +128,15 @@ function PUT_Todolist(){
     const titreElement = document.querySelector('#Title-of-Todo-list');
      btnModifier.addEventListener("click", function(event){
         event.preventDefault();
-        const id = n;
+        const id = journals[page].id;
         const arrayTarget = todoList.querySelectorAll("[name=box]");
         const arrayTargetTodo = todoList.querySelectorAll("[name=todo]");
         let arrayTodoList= []
-       
+        console.log(arrayTarget)
         for(let i = 0 ; i< arrayTarget.length ; i+=1){
            
             if(arrayTarget[i].checked != false){
-                arrayTodoList.push(journals[id].info[i]);
+                arrayTodoList.push(journals[page].info[i]);
                 arrayTodoList.splice(i,1,{id_todo : i ,tâches:arrayTargetTodo[i].value, status_termine : true});
             }
             else 
@@ -123,7 +148,8 @@ function PUT_Todolist(){
             info : arrayTodoList
         }
             const chargeUtile = JSON.stringify(todolist);
-            fetch(`http://localhost:8081/journal/${id}`,{
+    
+            fetch(` http://localhost:3000/journal/${id}`,{
                     method : "PUT",
                     headers : {"Content-Type": "application/json"},
                     body : chargeUtile 
@@ -131,27 +157,28 @@ function PUT_Todolist(){
     })                 
 }
 
+
 function DELETE_Todolist(){
-    let position =0;
     const todoList = document.querySelector(".createTodo");
     const btnSupprimer = document.querySelectorAll(".supprimer");
     const titreElement = document.querySelector('#Title-of-Todo-list');
     let arrayTodoList= []
-    console.log(btnSupprimer);
-    for(let u = 0 ; u<journals[n].info.length; u+=1){
-        arrayTodoList.push(journals[n].info[u]);
+    if(page >= 0 && page < journals.length){
+     const id = page+1
+    for(let u = 0 ; u<journals[page].info.length; u+=1){
+        arrayTodoList.push(journals[page].info[u]);
     }
-    for(let i = 0 ; i< journals[n].info.length ; i+=1){    
+    for(let i = 0 ; i< journals[page].info.length ; i+=1){    
     btnSupprimer[i].addEventListener("click", function(event){   
         event.preventDefault();
         arrayTodoList.splice(i,1);
         const todolist = {
-            id : n,
+            id : id,
             titre : titreElement.value,
             info : arrayTodoList
         }
             const chargeUtile = JSON.stringify(todolist);
-            fetch(`http://localhost:8081/journal/${n}`,{
+            fetch(` http://localhost:3000/journal/${id}`,{
                     method : "PUT",
                     headers : {"Content-Type": "application/json"},
                     body : chargeUtile 
@@ -159,53 +186,40 @@ function DELETE_Todolist(){
     })
     }     
 }
+}
 
 function PageSuivante()
 {
-    let s = parseInt(localStorage.getItem("idTitre"));
+ 
     const btnPlus =  document.querySelector(".categorie");
-    GET_Todolist(s);
-    btnPlus.addEventListener("click", function(event){
-           n+=1
-        if(n < journals.length)
-        {
-            event.preventDefault();
-             localStorage.setItem("idTitre", n)
-            document.querySelector(".todolistGenerate").innerHTML = "";
-            GET_Todolist(n)
-        }
-        else{
-            n=0;
-            localStorage.setItem("idTitre", n)
-            document.querySelector(".todolistGenerate").innerHTML = "";
-            GET_Todolist(n)
-        }
-       
+    btnPlus.addEventListener("click", function(){
+        let next = parseInt(localStorage.getItem("idTitre"));
+        next +=1;
+        document.querySelector(".todolistGenerate").innerHTML = "";
+        
+        localStorage.setItem("idTitre", next)
+        GET_Todolist(next);
+         location.reload();
     })
-  
+}
+
+function PagePrécente()
+{
+    const btnMoins =  document.querySelector(".categorie_gauche");
+    btnMoins.addEventListener("click", function(){
+        let next = parseInt(localStorage.getItem('idTitre'));
+        next -=1;
+        document.querySelector(".todolistGenerate").innerHTML = "";
+        GET_Todolist(next);
+        localStorage.setItem('idTitre', next); 
+        location.reload();
+    })
+
 }
 
 PageSuivante();
-EventaddTodoList();
 POST_Todolist();
 PUT_Todolist();
 DELETE_Todolist();
-
-
-// async function DeleteStain(){
-//     const divElements = document.querySelectorAll(".createTodo div button");
-//     // console.log(divElements);
-//     for(let i = 0 ; i < divElements.length ; i+=1){
-//         let a  = divElements[i].parentElement.querySelector('label').innerText;
-//         divElements[i].addEventListener("click", async function(event){
-//             console.log(event);
-//         const id =parseInt(event.target.dataset.id);
-//         event.preventDefault();
-//         const j = await fetch(`http://localhost:8081/journal/Title=/${a}`, {
-//          method : "DELETE",
-//          bod
-//         });
-//     });
-//   }
-// }
-// DeleteStain();
+PagePrécente();
+EventaddTodoList();
